7bb1d2808c76b5309f1f4f7d0cd67fdde33c10c6
saitohodaka <169113207+saitohodaka@users.noreply.github.com>
2024-06-28 15:10:46 +0900

    map tokyo

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ async function initMap(): Promise<void> {
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
   map = new Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 35.689, lng: 139.692 },
     zoom: 8,
   });
 }
