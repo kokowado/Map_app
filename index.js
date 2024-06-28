3ddde56555e360cc5991d36b718d8753a205d6a4
saitohodaka <169113207+saitohodaka@users.noreply.github.com>
2024-06-28 15:39:42 +0900

    map5

--- index.js
+++ index.js
@@ -10,4 +10,34 @@ async function initMap() {
   });
 }
 
-initMap();
+function initMap(){
+  const map=new google.maps.Map(document.getElementById("map")
+    zoom:3,
+    center: { lat: 35.689, lng: 139.692 },
+});
+const infoWindow =new google.maps.InforWindow({
+  content:"",
+  disableAutoPan:true,
+});
+
+const label="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const markers=locations.map((position, i) => {
+  const label =label[i% labels.lnegth];
+  const marker =new google.maps.Marker({
+    positon,
+    label,
+  });
+
+  marker.addListener("click",()=> {
+    infoWindow.setContent(label);
+    infoWindow.open(map,marker);
+  });
+  return marker;
+});
+
+new MarkerClusterer({markers,map});
+}
+
+    
+
+  
