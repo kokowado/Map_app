17a3a773146aa0afa5a7f381db5f0d4cf91b9401
saitohodaka <169113207+saitohodaka@users.noreply.github.com>
2024-06-30 11:45:31 +0900

    map21

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-/*let map;*/
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
-    center: { lat: 35.6895, lng: 139.6917 },
+    center: { lat: -28.024, lng: 140.887 },
   });
   const infoWindow = new google.maps.InfoWindow({
     content: "",
@@ -30,29 +29,31 @@ function initMap() {
   // Add a marker clusterer to manage the markers.
   new MarkerClusterer({ markers, map });
 }
-const locations = [
-　　{ lat: 35.6895, lng: 139.6917 },
-  　{ lat: 35.6586, lng: 139.7454 },
-    { lat: 35.6993, lng: 139.7710 },
-    { lat: 35.6733, lng: 139.7100 },
-    { lat: 35.6852, lng: 139.7528 },
-    { lat: 35.6957, lng: 139.7758 },
-    { lat: 35.6467, lng: 139.7090 },
-    { lat: 35.6674, lng: 139.7394 },
-    { lat: 35.6346, lng: 139.7390 },
-    { lat: 35.6894, lng: 139.7006 },
-    { lat: 35.7023, lng: 139.7745 },
-    { lat: 35.7101, lng: 139.8107 },
-    { lat: 35.6544, lng: 139.7591 },
-    { lat: 35.6580, lng: 139.7516 },
-    { lat: 35.6482, lng: 139.7100 },
-    { lat: 35.6695, lng: 139.7617 },
-    { lat: 35.6764, lng: 139.6993 },
-    { lat: 35.6714, lng: 139.7630 },
-    { lat: 35.7044, lng: 139.7602 },
-    { lat: 35.7120, lng: 139.7788 },
-    { lat: 35.6909, lng: 139.7003 },
-    { lat: 35.6654, lng: 139.7389 }
-];    
 
-windows.initMap = initMap;
+const locations = [
+  { lat: -31.56391, lng: 147.154312 },
+  { lat: -33.718234, lng: 150.363181 },
+  { lat: -33.727111, lng: 150.371124 },
+  { lat: -33.848588, lng: 151.209834 },
+  { lat: -33.851702, lng: 151.216968 },
+  { lat: -34.671264, lng: 150.863657 },
+  { lat: -35.304724, lng: 148.662905 },
+  { lat: -36.817685, lng: 175.699196 },
+  { lat: -36.828611, lng: 175.790222 },
+  { lat: -37.75, lng: 145.116667 },
+  { lat: -37.759859, lng: 145.128708 },
+  { lat: -37.765015, lng: 145.133858 },
+  { lat: -37.770104, lng: 145.143299 },
+  { lat: -37.7737, lng: 145.145187 },
+  { lat: -37.774785, lng: 145.137978 },
+  { lat: -37.819616, lng: 144.968119 },
+  { lat: -38.330766, lng: 144.695692 },
+  { lat: -39.927193, lng: 175.053218 },
+  { lat: -41.330162, lng: 174.865694 },
+  { lat: -42.734358, lng: 147.439506 },
+  { lat: -42.734358, lng: 147.501315 },
+  { lat: -42.735258, lng: 147.438 },
+  { lat: -43.999792, lng: 170.463352 },
+];
+
+window.initMap = initMap;
