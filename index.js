6e1157892057144198b5fbabb74d216b86579140
saitohodaka <169113207+saitohodaka@users.noreply.github.com>
2024-06-30 23:57:38 +0900

    40

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ async function initMap() {
 
 initMap();
 
-/*
 // This example displays a marker at the center of Australia.
 // When the user clicks the marker, an info window opens.
 function initMap() {
@@ -75,4 +74,4 @@ function initMap() {
   });
 }
 window.initMap = initMap;
-*/
+
